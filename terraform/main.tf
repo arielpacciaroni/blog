33f297df7fcf777746ce5dbb6118655cb250ce92
Ariel Pacciaroni <29680560+arielpacciaroni@users.noreply.github.com>
2023-02-17 22:15:21 -0300

    Add Terraform (#1)
    * Add Terraform
    
    * Rename secrets and workflow name
    
    * Improved steps name, created bucket
    
    * Improved steps name, created bucket
    
    * Fix variables
    
    ---------
    
    Co-authored-by: arielpacciaroni <29680560+batidodecoco@users.noreply.github.com>

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.3.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.54.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-blog-state"
+    key    = "blog.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
